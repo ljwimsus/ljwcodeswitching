d0d0f05fc61bf4cc302069864789c867ef4404ae
ljwimsus <ljwimsus@gmail.com>
2017-12-22 05:33:37 +0800

    add SEAME and KALDI paths to paths.sh

--- ljwcodeswitching/path.sh
+++ ljwcodeswitching/path.sh
@@ -4,3 +4,6 @@ export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 . $KALDI_ROOT/tools/config/common_path.sh
 export LC_ALL=C
+
+export SEAME_path=`pwd`/SEAME
+export KALDI_path=`pwd`/KALDI
