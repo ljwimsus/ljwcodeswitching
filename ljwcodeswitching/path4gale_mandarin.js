2b649961887137d2c5c1045885f7242f97f95308
ljwimsus <ljwimsus@gmail.com>
2018-01-05 01:51:10 +0800

    create in eclipse which will link to gale_mandarin

---
+++ ljwcodeswitching/path4gale_mandarin.js
@@ -0,0 +1,15 @@
+/**
+ * create in eclipse which will link to gale_mandarin
+ */
+
+export KALDI_ROOT=~/kaldi/kaldi
+export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/kaldi_lm:$PWD:$PATH
+[ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
+# :' . $KALDI_ROOT/tools/config/common_path.sh '
+# ＃ . $KALDI_ROOT/tools/env.sh #＃ is a Chinese character ＃ not ANSCI #
+. $KALDI_ROOT/tools/config/common_path.sh
+. $KALDI_ROOT/tools/env.sh
+
+export LC_ALL=C
+
+echo "end of path.sh"
