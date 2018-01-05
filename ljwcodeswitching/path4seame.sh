8ef85a460ef523e011a82835ee19fe9ba63b3993
ljwimsus <ljwimsus@gmail.com>
2018-01-06 03:13:31 +0800

    add space to
    echo "    call of env.sh"

--- ljwcodeswitching/path4seame.sh
+++ ljwcodeswitching/path4seame.sh
@@ -6,10 +6,10 @@ export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$KALDI_ROOT/tools/kaldi_lm
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
 # :' . $KALDI_ROOT/tools/config/common_path.sh '
 # ＃ . $KALDI_ROOT/tools/env.sh #＃ is a Chinese character ＃ not ANSCI #
-echo "call of common_path.sh"
+echo "    call of common_path.sh"
 . $KALDI_ROOT/tools/config/common_path.sh
 echo "end of common_path.sh"
-echo "call of env.sh"
+echo "    call of env.sh"
 . $KALDI_ROOT/tools/env.sh
 echo "end of env.sh"
 
