fae914aa7f2f9932c2b4985bcf57e243ee00d061
ljwimsus <ljwimsus@gmail.com>
2018-01-06 02:43:08 +0800

    modified previous gale_mandarin settings

--- ljwcodeswitching/path4seame.sh
+++ ljwcodeswitching/path4seame.sh
@@ -1,13 +1,3 @@
-#/**
-# * create in eclipse which will link to gale_mandarin/path.sh
-# */
-
-#./path.sh: line 1: /Applications: is a directory
-#./path.sh: line 2: RESULTS: command not found
-#./path.sh: line 3: conf/: is a directory
-
-# the above is the console error msg for unfix .js /* */ comments
- 
 echo "begin of path.sh"
 
 export KALDI_ROOT=~/kaldi/kaldi
@@ -24,6 +14,4 @@ echo "end of env.sh"
 
 export LC_ALL=C
 
-echo "end of path.sh"
-
-echo "actually run from path4gale_mandarin.sh(previous path4gale_mandarin.js)"
+echo "end of path.sh (actually run from $0)"
