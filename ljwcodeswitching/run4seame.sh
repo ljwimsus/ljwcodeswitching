3e121d04bb9df7407c481680fb20aadc09c63664
ljwimsus <ljwimsus@gmail.com>
2018-01-06 06:41:13 +0800

    echo "run4seame.sh begins (actually run by $0) on: " & date;

--- ljwcodeswitching/run4seame.sh
+++ ljwcodeswitching/run4seame.sh
@@ -6,7 +6,7 @@
 echo "";
 echo "";
 
-echo "run4seame.sh begins (actually run from $0) on: " & date;
+echo "run4seame.sh begins (actually run by $0) on: " & date;
 
 step=$1;
 
@@ -261,5 +261,5 @@ exit 0
 
 esac
 
-echo "run4seame.sh ends (actually run from $0) on: " & date;
+echo "run4seame.sh ends (actually run by $0) on: " & date;
 echo "";
